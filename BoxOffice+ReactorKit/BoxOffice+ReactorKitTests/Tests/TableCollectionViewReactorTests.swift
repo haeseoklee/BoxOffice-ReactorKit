ff5ac4f66122caac66293b79b0c9464464c800b7
haeseoklee <haeseoklee.dev@gmail.com>
2022-01-21 16:07:39 +0900

    [#57] TableCollectionViewReactorTests 구현

--- BoxOffice+ReactorKit/BoxOffice+ReactorKitTests/Tests/TableCollectionViewReactorTests.swift
+++ BoxOffice+ReactorKit/BoxOffice+ReactorKitTests/Tests/TableCollectionViewReactorTests.swift
@@ -8,6 +8,11 @@
 import XCTest
 @testable import BoxOffice_ReactorKit
 
+import ReactorKit
+import RxSwift
+import RxCocoa
+import RxTest
+
 class TableCollectionViewReactorTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -17,20 +22,107 @@ class TableCollectionViewReactorTests: XCTestCase {
     override func tearDownWithError() throws {
         try super.tearDownWithError()
     }
-
-    func testAction_refresh() {
+    
+    func testStateIsActivated() {
         // given
-        let movieService = MovieService()
+        let movieService = MockMovieListService()
         let reactor = TableCollectionViewReactor(movieService: movieService)
-        reactor.isStubEnabled = true
-        
-        let view = TableViewController()
+        let scheduler = TestScheduler(initialClock: 0)
+        let disposeBag = DisposeBag()
         
         // when
-        
-        
+        scheduler
+            .createHotObservable([
+                .next(100, .fetchMovies),
+            ])
+            .subscribe(reactor.action)
+            .disposed(by: disposeBag)
         
         // then
+        let response = scheduler.start(created: 0, subscribed: 0, disposed: 1000) {
+            reactor.state.map(\.isActivated)
+        }
+        
+        XCTAssertEqual(response.events.compactMap(\.value.element), [
+            false, // initialValue
+            true,  // setIsActivated(true)
+            true,  // setSections
+            false  // setIsActivated(false)
+        ])
+    }
+    
+    func testStateOrderType() {
+        // given
+        let movieService = MockMovieListService()
+        let reactor = TableCollectionViewReactor(movieService: movieService)
+        
+        
+        // when
+        reactor.action.onNext(.changeOrderType(.reservationRate))
+        reactor.action.onNext(.changeOrderType(.openingDate))
+        reactor.action.onNext(.changeOrderType(.curation))
+        
+        // then
+        XCTAssertEqual(reactor.currentState.orderType, .curation)
     }
 
+    func testStateSections() {
+        // given
+        let movieService = MockMovieListService()
+        let reactor = TableCollectionViewReactor(movieService: movieService)
+        let scheduler = TestScheduler(initialClock: 0)
+        let disposeBag = DisposeBag()
+        
+        // when
+        scheduler
+            .createHotObservable([
+                .next(100, .fetchMovies),
+            ])
+            .subscribe(reactor.action)
+            .disposed(by: disposeBag)
+        
+        // then
+        scheduler.start()
+        XCTAssertEqual(reactor.currentState.sections[0].items.count, 3)
+    }
+    
+    func testStateIsErrorOccured() {
+        // given
+        let movieService = MockMovieListService()
+        let reactor = TableCollectionViewReactor(movieService: movieService)
+        let scheduler = TestScheduler(initialClock: 0)
+        let disposeBag = DisposeBag()
+        
+        // when
+        scheduler
+            .createHotObservable([
+                .next(100, .fetchMovies),
+            ])
+            .subscribe(reactor.action)
+            .disposed(by: disposeBag)
+        
+        // then
+        scheduler.start()
+        XCTAssertEqual(reactor.currentState.isErrorOccurred, false)
+    }
+    
+    func testStateError() {
+        // given
+        let movieService = MockMovieListService()
+        let reactor = TableCollectionViewReactor(movieService: movieService)
+        let scheduler = TestScheduler(initialClock: 0)
+        let disposeBag = DisposeBag()
+        
+        // when
+        scheduler
+            .createHotObservable([
+                .next(100, .fetchMovies),
+            ])
+            .subscribe(reactor.action)
+            .disposed(by: disposeBag)
+        
+        // then
+        scheduler.start()
+        XCTAssertEqual(reactor.currentState.error, nil)
+    }
 }
